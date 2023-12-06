4dfde9716d8cfe3804cec579138023c29ca024aa
chaitanyareddyk <chaitanyareddy.kothapeta@gmail.com>
2023-12-06 11:04:16 +0530

    removed unused env var

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -8,7 +8,6 @@ class EnvDetails:
     DATABASE_USER = os.getenv("DATABASE_USER")
     DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
     DATABASE_HOST = os.getenv("DATABASE_HOST")
-    DATABASE_PORT = os.getenv("DATABASE_PORT")
     DATABASE_NAME = os.getenv("DATABASE_NAME")
     REDIS_URL = os.getenv("REDIS_URL")
 
